7ac8e296953c265f1891615664a7695844b1785d
Maachue <maachue@outlook.com>
2025-08-29 20:28:02 +0700

    Change & Add: bash things

--- .bashrc.d/05-aliases.sh
+++ .bashrc.d/05-aliases.sh
@@ -1,4 +1,8 @@
 alias ls='eza -la --icons'
 alias bonsai='cbonsai --life 40 --multiplier 5 --time 20 --screensaver'
-alias niri-conf='nvim ~/.config/niri/'
-alias waybar-conf='cd ~/.config/waybar/'
+
+alias install='sudo dnf install'
+alias update='sudo dnf upgrade'
+alias wat='dnf check-upgrade'
+alias remove='sudo dnf remove'
+alias search='dnf search'
---
+++ .bashrc.d/10-prompt.sh
@@ -0,0 +1,2 @@
+# Oh-My-Posh
+eval "$(oh-my-posh init bash --config $HOME/.config/oh-my-posh/zen.toml)"
